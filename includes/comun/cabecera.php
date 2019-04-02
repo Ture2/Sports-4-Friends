6ae85a577628d59ecb729176ae4d161a51874f93
Alberto Turégano Castedo <alberto_tureganocsb@hotmail.com>
2019-04-02 18:37:23 +0200

    Más archivos

---
+++ includes/comun/cabecera.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/estilo.css" />
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<div id="cabecera">
+		<div id="logoC">
+			<img class="logoC" src="images/logo.png">
+		</div>
+	</div>
+</body>
+</html>
---
+++ includes/comun/pie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<div id="pie">
+		<div id="icon">
+			<img id="icon" src="images/facebook.png">
+			<img id="icon" src="images/twitter.png">
+			<img id="icon" src="images/instagram.png">
+			<a id="pie" href="">Contacto</a>
+			
+		</div>
+	</div>
+</body>
+</html>
