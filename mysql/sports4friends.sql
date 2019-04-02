965bd53a8776aaf85e119a379aa1ec8e13b5e47c
alvarocarpi94 <35227586+alvarocarpi94@users.noreply.github.com>
2019-04-02 21:59:06 +0200

    MODIFICACION BASE DE DATOS
    Ahora mismo solo hay datos insertados en la tabla usuarios, para comprobar el funcionamiento de login.

--- mysql/sports4friends.sql
+++ mysql/sports4friends.sql
@@ -1,3 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-04-2019 a las 21:57:37
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -26,18 +34,9 @@ CREATE TABLE `deportes` (
   `ID_DEPORTE` int(10) NOT NULL,
   `NOMBRE_DEPORTE` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `NUMERO_MAXIMO_JUGADORES` int(3) DEFAULT NULL,
-  `DURACION_MIN` int(5) DEFAULT NULL
+  `DURACION_MIN` int(5) DEFAULT NULL,
+  `FECHA` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `deportes`
---
-
-INSERT INTO `deportes` (`ID_DEPORTE`, `NOMBRE_DEPORTE`, `NUMERO_MAXIMO_JUGADORES`, `DURACION_MIN`) VALUES
-(880001, 'BALONCESTO', 10, 40),
-(880002, 'FUTBOL', 20, 90),
-(880003, 'BEISBOL', 10, 60),
-(880004, 'BALONMANO', 15, 40);
 
 -- --------------------------------------------------------
 
@@ -47,19 +46,34 @@ INSERT INTO `deportes` (`ID_DEPORTE`, `NOMBRE_DEPORTE`, `NUMERO_MAXIMO_JUGADORES
 
 CREATE TABLE `equipos` (
   `ID_EQUIPO` int(10) NOT NULL,
-  `DEPORTE` int(10) DEFAULT NULL,
-  `NOMBRE_EQUIPO` varchar(30) COLLATE utf8_spanish_ci NOT NULL
+  `DEPORTE` int(10) NOT NULL,
+  `NOMBRE_EQUIPO` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `FECHA` date NOT NULL,
+  `PARTIDOS_GANADOS` int(11) NOT NULL DEFAULT '0',
+  `PARTIDOS_PERDIDOS` int(11) NOT NULL DEFAULT '0',
+  `PARTIDOS_EMPATADOS` int(11) NOT NULL DEFAULT '0',
+  `LOGO` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `DESCRIPCION` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `DESCRIPCION_EQUIPO` varchar(200) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `equipos`
+-- Estructura de tabla para la tabla `eventos`
 --
 
-INSERT INTO `equipos` (`ID_EQUIPO`, `DEPORTE`, `NOMBRE_EQUIPO`) VALUES
-(550001, 880001, 'BULL'),
-(550002, 880002, 'REAL MADRID'),
-(550003, 880003, 'SALOU'),
-(550004, 880001, 'VALENCIA');
+CREATE TABLE `eventos` (
+  `ID_EVENTO` int(11) NOT NULL,
+  `NOMBRE` varchar(45) NOT NULL,
+  `LOCALIZACION` varchar(100) NOT NULL,
+  `TIPO_DEPORTE` varchar(45) NOT NULL,
+  `DESCRIPCION` varchar(100) DEFAULT NULL,
+  `EQUIPO_1` varchar(45) NOT NULL,
+  `EQUIPO_2` varchar(45) DEFAULT NULL,
+  `PORCENTAJE` decimal(10,2) NOT NULL,
+  `FECHA_CREACION` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -69,33 +83,11 @@ INSERT INTO `equipos` (`ID_EQUIPO`, `DEPORTE`, `NOMBRE_EQUIPO`) VALUES
 
 CREATE TABLE `jugadores` (
   `ID_JUGADOR` int(10) NOT NULL,
-  `EQUIPO` int(10) DEFAULT NULL,
-  `USUARIO` int(10) DEFAULT NULL,
-  `Admin_team` tinyint(4) NOT NULL DEFAULT '0'
+  `EQUIPO` int(10) NOT NULL,
+  `USUARIO` int(10) NOT NULL,
+  `ROL_JUGADOR` tinyint(1) NOT NULL DEFAULT '0',
+  `FECHA` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `jugadores`
---
-
-INSERT INTO `jugadores` (`ID_JUGADOR`, `EQUIPO`, `USUARIO`, `Admin_team`) VALUES
-(330001, 550001, 1, 1),
-(330002, 550001, 2, 0),
-(330003, 550002, 3, 1),
-(330004, 550002, 4, 0),
-(330005, 550003, 5, 1),
-(330006, 550003, 6, 0),
-(330007, 550001, 7, 0),
-(330008, 550001, 8, 0),
-(330009, 550001, 9, 0),
-(330010, 550002, 10, 0),
-(330011, 550002, 11, 0),
-(330012, 550002, 12, 0),
-(330013, 550002, 13, 0),
-(330014, 550003, 14, 0),
-(330015, 550003, 15, 0),
-(330016, 550003, 16, 0),
-(330017, 550003, 17, 0);
 
 -- --------------------------------------------------------
 
@@ -105,36 +97,25 @@ INSERT INTO `jugadores` (`ID_JUGADOR`, `EQUIPO`, `USUARIO`, `Admin_team`) VALUES
 
 CREATE TABLE `usuarios` (
   `ID_USUARIO` int(10) NOT NULL,
-  `NICKNAME` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
-  `NOMBRE` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
-  `APELLIDOS` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
-  `TELEFONO` int(9) DEFAULT NULL,
-  `CORREO` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `PASSWORD` varchar(100) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `NICKNAME` varchar(15) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `NOMBRE` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `CORREO` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `PASSWORD` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `ROL_USUARIO` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL DEFAULT 'USER'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`ID_USUARIO`, `NICKNAME`, `NOMBRE`, `APELLIDOS`, `TELEFONO`, `CORREO`, `PASSWORD`) VALUES
-(1, 'alv1', 'Alvaro', 'Carpizo Garcia', 611111111, 'alvcarpi@ucm', '1111'),
-(2, 'jhce2', 'Jhimmy Ender', 'Candela Espinosa', 622222222, 'jcandela@ucm.es', '2222'),
-(3, 'jvp3', 'Jorge', 'Valsameda Plasencia', 633333333, 'jvalma01@ucm.es', '3333'),
-(4, 'iif4', 'Iker', 'Ibañez FLores', 644444444, 'ikeriban@ucm.es', '4444'),
-(5, 'atc5', 'Alberto', 'Turegano Castedo', 655555555, 'alberture@ucm.es', '5555'),
-(6, 'jjjj6', 'Jonathan Jose', 'Jimenez Jimenez', 666666666, 'jonathanj@ucm.es', '6666'),
-(7, 'fff7', 'Federico', 'Garcia Muñoz', 777777777, 'federico@ucm.es', '7777'),
-(8, 'ale45', 'Alejandro', 'Rivas Moreira', 647282865, 'alerivas@ucm.es', 'sodf1234'),
-(9, 'adr', 'Andrea Sthephania', 'Janampa Nebrica', 675820123, 'Andreasthe@ucm.es', '1234jii'),
-(10, 'anton', 'Antonio ', 'Rodiriguez', 693874739, 'Antonioro@ucm.es', 'ciin585'),
-(11, 'charly', 'Carlos', 'Cervigon', 685894939, 'Carlocer@ucm.es', 'sf3€'),
-(12, 'mia', 'Cintia', 'Perraño Ruiz', 685930022, 'Cintiapry@ucm.es', 'lsdfo'),
-(13, 'bull', 'Cristian ', 'Molina Muñoz', 674828382, 'Cristianm@ucm.es', '234qwer'),
-(14, 'fan', 'Daniel', 'Jorguera', 685393434, 'Danialjord@ucm.es', '1234'),
-(15, 'user', 'Hector', 'Mangarran', 688493454, 'Hector@ucm.es', '12342'),
-(16, 'jenni', 'Jennifer ', 'Marmorejos', 658200304, 'Jennyfer4@ucm.es', 'jeny1234'),
-(17, 'hacking', 'Jokin', 'Garde ', 657392939, 'Jockinga@ucm.es', '11234');
+INSERT INTO `usuarios` (`ID_USUARIO`, `NICKNAME`, `NOMBRE`, `CORREO`, `PASSWORD`, `ROL_USUARIO`) VALUES
+(41, 'admin', 'admin', 'admin@ucm', 'adminpass', 'ADMIN'),
+(42, 'alv1', 'Alvaro', 'alvcarpi@ucm', '1111', 'USER'),
+(43, 'jhce2', 'Jhimmy Ender', 'jcandela@ucm.es', '2222', 'USER'),
+(44, 'jvp3', 'Jorge', 'jvalma01@ucm.es', '3333', 'USER'),
+(45, 'iif4', 'Iker', 'ikeriban@ucm.es', '4444', 'USER'),
+(46, 'atc5', 'Alberto', 'alberture@ucm.es', '5555', 'USER'),
+(47, 'jjjj6', 'Jonathan Jose', 'jonathanj@ucm.es', '6666', 'USER');
 
 --
 -- Índices para tablas volcadas
@@ -154,6 +135,12 @@ ALTER TABLE `equipos`
   ADD PRIMARY KEY (`ID_EQUIPO`),
   ADD UNIQUE KEY `NOMBRE_EQUIPO` (`NOMBRE_EQUIPO`),
   ADD KEY `DEPORTE` (`DEPORTE`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`ID_EVENTO`);
 
 --
 -- Indices de la tabla `jugadores`
@@ -188,6 +175,12 @@ ALTER TABLE `equipos`
   MODIFY `ID_EQUIPO` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=550005;
 
 --
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `ID_EVENTO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `jugadores`
 --
 ALTER TABLE `jugadores`
@@ -197,7 +190,7 @@ ALTER TABLE `jugadores`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `ID_USUARIO` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID_USUARIO` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- Restricciones para tablas volcadas
