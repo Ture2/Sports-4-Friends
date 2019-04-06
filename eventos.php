6151e583d88d47c7d045126b062f5cb47f6cc950
HackingSecurity <32521125+HackingSecurity@users.noreply.github.com>
2019-04-06 19:53:37 +0200

    Update eventos.php

--- eventos.php
+++ eventos.php
@@ -19,9 +19,7 @@ if(isset($_SESSION["login"])){
 else{
 	$errores[] = "No puedes acceder al contenido";
 }
-		
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -38,23 +36,58 @@ else{
 
 	<div id= "contenido">
 
-		<?phps
+		<?php
 		if (isset($_SESSION["login"]))
 		{
-		?>
-		<h1>TORNEO 3V3 DE BALONCESTO </h1>
-		<img id="evento1" src="images/evento1.jpg">evento1_3vs3_baloncesto</img>
+			if($errores == 0)
+			{
+			?>
+				<h1>TORNEO 3V3 DE BALONCESTO </h1>
+				<img id="evento1" src="images/evento1.jpg">evento1_3vs3_baloncesto</img>
 
-		<p>Solo pueden inscribirsen los lideres de los equipos. Si no tienes equipo y quieres participar
-			crear un <a href="crearEquipo.php">EQUIPO</a> y reune amigos para participar (minimo 3 personas)
-		</p>
+				<p>Solo pueden inscribirsen los lideres de los equipos. Si no tienes equipo y quieres participar
+					crear un <a href="crearEquipo.php">EQUIPO</a> y reune amigos para participar (minimo 3 personas)
 
+				<!--INDENTIFICADO PARA EL CSS -->
+				<table id="eventos">
+					<caption>LISTA DE LOS EVENTOS DISPONIBLES</caption>
+						</thead>
+							<tr>
+								<th>Numero</th>
+								<th>Nombre del Evento</th>
+								<th>Equipo</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								/*
+								1)Recorremos todo el array.
+								2)Accedemos a nuestro objeto (valor) y a los metodos
+								3)Mostramos lo que nos retorna los metodos
+								*/
+								foreach ($eventos as $key => $value) {
+								?>
+									<tr>
+										<td> <?php echo $value->id() ?> </td>
+										<td> <?php echo $value->nombre() ?> </td>
+										<td> <?php echo $value->tipo_deporte() ?> </td>
+								<?php
+								}
+							?>
+						</tbody>
+				</table>
 		<?php
+			}	
+			else
+			{
+				echo $errores()
+			}
 		}
 		else{
 		?>
-		<p>Debes hacer <a href="login.php">login</a> o <a 	href="registro.php">registrarte</a> para
-		poder ver el contenido de los PROXIMOS EVENTOS DE TU CIUDAD </p>
+			<p>Debes hacer <a href="login.php">login</a> o <a 	href="registro.php">registrarte</a> para
+			poder ver el contenido de los PROXIMOS EVENTOS DE TU CIUDAD </p>
+
 		<?php
 		}
 		?>
