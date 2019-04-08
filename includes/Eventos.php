17e1756fc5eb92e80ba62f4acac5d311471978cd
HackingSecurity <32521125+HackingSecurity@users.noreply.github.com>
2019-04-09 00:25:06 +0200

    eventos terminada

--- includes/Eventos.php
+++ includes/Eventos.php
@@ -48,18 +48,21 @@ class Eventos
                 2)el objecto es una de las tuplas devueltas de la consulta
                 3)se instancia el objeto con el orden de los campos sport4friends.evento
                 */
-                $result[] = new Eventos($row['id_evento'], 
-                                        $row['nombre_evento'],
-                                        $row['deporte'],
-                                        $row['ciudad'],
-                                        $row['municipio'],
-                                        $row['localizacion'],
-                                        $row['fecha_creacion'],
-                                        $row['fecha_evento'],
-                                        $row['hora_evento'],
-                                        $row['descripcion'],
-                                        $row['foto_evento']);
-                        }
+                $evento = new Eventos($row['nombre_evento'],
+                                     $row['deporte'],
+                                     $row['ciudad'],
+                                     $row['municipio'],
+                                     $row['localizacion'],
+                                     $row['fecha_creacion'],
+                                     $row['fecha_evento'],
+                                     $row['hora_evento'],
+                                     $row['descripcion'],
+                                     $row['foto_evento']);
+
+                $evento->id_evento = $row['id_evento'];
+
+                $result[] = $evento;
+            }
             $rs->free();
         } 
         else {
@@ -67,6 +70,100 @@ class Eventos
             exit();
         }
         return $result;
+    }
+
+    public static function crea($nombre_evento, $deporte, $ciudad, $municipio, $localizacion, $fecha_creacion, $fecha_evento, $hora_evento, $descripcion, $foto_evento)
+    {
+        $evento = self::buscarEvento($nombre_evento;
+        if ($evento) {
+            return false;
+        }
+        $evento = new Eventos($nombre_evento, $deporte, $ciudad, $municipio, $localizacion, $fecha_creacion, $fecha_evento, $hora_evento, $descripcion, $foto_evento);
+
+        return self::guardarEvento($evento);
+    }
+
+    public static function buscarEvento($nombre_evento)
+    {
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query = sprintf("SELECT * FROM Eventos E WHERE E.nombre_evento= '%s'", $conn->real_escape_string($nombre_evento));
+        
+        $result = false; 
+
+        if (($rs = $conn->query($query))  && ($rs->num_rows == 1))
+        {   
+            $row = $rs->fetch_assoc();
+            $evento = new Eventos($row['nombre_evento'],$row['deporte'], $row['ciudad'],$row['municipio'],$row['localizacion'],$row['fecha_creacion'],$row['fecha_evento'],$row['hora_evento'],$row['descripcion'],$row['foto_evento']);
+            $evento->id_evento = $row['id_evento'];
+            $result[] = $evento;
+        }
+        $rs->free();
+        } 
+        else {
+            echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $result;
+
+     public static function guardarEvento($evento)
+    {
+        if ($evento->id_evento !== null) {
+            return self::actualizarEvento($evento);
+        }
+        return self::insertarEvento($evento);
+    }
+
+    private static function insertarEvento($evento)
+    {
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query=sprintf("INSERT INTO Eventos(nombre_evento, deporte, ciudad, municipio, localizacion, fecha_creacion, fecha_evento, hora_evento, descripcion, foto_evento) VALUES('%s', '%s', '%s', '%s', '%s','%s', '%s', '%s', '%s', '%s')"
+            , $conn->real_escape_string($evento->nombre_evento)
+            , $conn->real_escape_string($evento->deporte)
+            , $conn->real_escape_string($evento->ciudad)
+            , $conn->real_escape_string($evento->municipio)
+            , $conn->real_escape_string($evento->localizacion)
+            , $conn->real_escape_string($evento->fecha_creacion)
+            , $conn->real_escape_string($evento->fecha_evento)
+            , $conn->real_escape_string($evento->hora_evento)
+            , $conn->real_escape_string($evento->descripcion)
+            , $conn->real_escape_string($evento->foto_evento));
+        if ( $conn->query($query) ) {
+            $evento->id = $conn->insert_id;
+        } else {
+            echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $evento;
+    }
+    
+    private static function actualizarEvento($evento)
+    {
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query=sprintf("UPDATE Eventos E SET nombre_evento = '%s', deporte='%s', ciudad='%s', municipio='%s', localizacion='%',fecha_creacion='%', fecha_evento='%', hora_evento='%',descripcion='%', foto_evento='%' WHERE U.id_evento=%i"
+            , $conn->real_escape_string($evento->nombre_evento)
+            , $conn->real_escape_string($evento->deporte)
+            , $conn->real_escape_string($evento->ciudad)
+            , $conn->real_escape_string($evento->municipio)
+            , $conn->real_escape_string($evento->localizacion)
+            , $conn->real_escape_string($evento->fecha_creacion)
+            , $conn->real_escape_string($evento->fecha_evento)
+            , $conn->real_escape_string($evento->hora_evento)
+            , $conn->real_escape_string($evento->descripcion)
+            , $conn->real_escape_string($evento->foto_evento)
+            , $evento->id_evento);
+        if ( $conn->query($query) ) {
+            if ( $conn->affected_rows != 1) {
+                echo "No se ha podido actualizar el evento: " . $evento->id;
+                exit();
+            }
+        } else {
+            echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $evento;
     }
 
     //************************************************************************
@@ -88,9 +185,8 @@ class Eventos
     
 
     //Constructora
-    private function __construct($id_evento, $nombre_evento, $deporte, $ciudad, $municipio, $localizacion, $fecha_creacion, $fecha_evento, $hora_evento, $descripcion, $foto_evento)
+    private function __construct($nombre_evento, $deporte, $ciudad, $municipio, $localizacion, $fecha_creacion, $fecha_evento, $hora_evento, $descripcion, $foto_evento)
     {
-        $this->id_evento=$id_evento;
         $this->nombre_evento= $nombre_evento;
         $this->deporte= $deporte;
         $this->ciudad=$ciudad;
