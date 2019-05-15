e0cfa8431a5db22d6cce25db583c7e9d15b2448f
jotajjjj <jose@JJ>
2019-05-15 11:09:11 +0200

    crear quedada, apuntarse, abanndonar hecho a falta de testear a tope

--- quedadas.php
+++ quedadas.php
@@ -1,3 +1,30 @@
+<?php
+require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/Quedada.php';
+require_once __DIR__.'/includes/Usuario.php';
+require_once __DIR__.'/includes/Invitado.php';
+$errores = array();
+
+
+    $quedadas = Quedada::getAllQuedadas();
+
+
+    if(!isset($_SESSION['login']))
+     $errores[]="Registrate para apuntarte a una quedada";
+    
+     if(count($quedadas)==0){
+         $errores[] = "No hay quedadas registradas";
+     }
+
+
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,38 +39,73 @@
 	<div id= "contenido">
 
 		<?php
+
+		// si hay una sesion activa
 		if (isset($_SESSION["login"])){
-				echo "<div id='quedadas'>";
-					echo "<h1 id='h1'>Bar Manolo</h1>";
-					echo "<img id='quedada' src='images/bar.jpg'></img>";
-					echo "<pre id='texto1'>Tu bar de confianza.</pre>";
-				echo "</div>";
+                
+		    echo "<form>";
+		    echo "<button formaction='crearQuedada.php' class='login-equipos'>Pulsa aqui para crear una quedada</button>";
+		    echo "</form>";
+		    
+			if(count($errores) == 0){
+                
 
-				echo "<div id='quedadas'>";
-					echo "<h1 id='h1'>Parque de la plaza</h1>";
-					echo "<img id='quedada' src='images/parque.jpg'></img>";
-					echo "<pre id='texto1'>Parque situado en la calle falsa 123.</pre>";
-				echo "</div>";
+				foreach ($quedadas as  $value) {
+				// vamos a hacerlo todo con id de queadadas
+				  $creador=Usuario::buscaUsuarioPorId($value->creador());  
+				  
+				  $num_invitados=Invitado::getInvitadosPorQuedada($value);
+				  
+				  $num_invitados=count($num_invitados);
+				  
+				  ?>
+				<div id="quedadas">
+					<h1 id="h"><?=$value->nombre_quedada();?></h1>
+					<img id="quedada" src="<?=$value->ruta_foto();?>"></img>
+					<pre id=texto><?=  "DESCRIPCION: ".$value->descripcion();?></pre>
+					<pre id=texto><?= "CREADOR: " .$creador->nicknameUsuario()?></pre>
+					<pre id=texto><?=$value->ciudad();?></pre>
+					<pre id=texto><?=$value->fecha_quedada();?></pre>
+					<pre id=texto><?="NUMERO DE ASISTENTES: ".$num_invitados?></pre>
+					<pre id=texto><?="HORA DE QUEADADA ".$value->hora_quedada();?></pre>
+					<div>
+						<a href="pantallaQuedada.php?id_quedada=<?php echo $value->id_quedada();?>"><button class ="login-equipos" >INFO QUEDADA</button>></a>
+						</div>
+				</div>
+				<?php
+				}
+				?>
 
-				echo "<div id='quedadas'>";
-					echo "<h1 id='h1'>Estadio Imperial</h1>";
-					echo "<img id='quedada' src='images/estadio.jpg'></img>";
-					echo "<pre id='texto1'>El polideportivo favorito de los deportistas.</pre>";
-				echo "</div>";
+		<?php
+			}	
+			else
+			{
+						  //muestra todos los posibles errores  
+			    foreach ($errores as $error){
+			        echo "<p>$error</p>";
+			    }
+			}
 		}
 		else{
-			echo "<div id='errorQuedada'>";
-				echo "<h1 id='h'>NO PUEDES ACCEDER AL CONTENIDO</h1>";
-				echo "<div id='errorQuedada2'>";
-				echo "<form>";
-					echo "<button formaction='login.php' type='submit' class='login-equipos'>INICIAR SESIÓN</button></a>";
-					echo "<button formaction='registro.php' type='submit' class='login-equipos'>REGISTRO</button></a>";
-					echo "<button formaction='index.php' type='submit' class='login-equipos'>VOLVER</button></a>";
-				echo "</form>";
-				echo "</div>";
-			echo "</div>";
+		?>
+		
+	
+		
+		
+			<div id="errorQuedada">
+				<h1 id="h"> <?php print $errores['0'];?></h1>
+				<div id="errorEvento2">
+					<form>
+						<button formaction='login.php' type='submit' class='login-equipos'>INICIAR SESIÓN</button></a>
+						<button formaction='registro.php' type='submit' class='login-equipos'>REGISTRO</button></a>
+						<button formaction='index.php' type='submit' class='login-equipos'>VOLVER</button></a>
+					</form>
+				</div>
+			</div>
+		<?php
 		}
 		?>
+
 	</div>
 
 	<?php
