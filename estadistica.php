fe5926759152e20dc12eb7fea8343f7ca0619f39
jvalma <jvalma01@ucm.es>
2019-05-16 12:46:59 +0200

    Estadisticas Implementado
    Clase Padre Estadistica, Clases hijas: futnol, Baloncesto, Balonmano, Beisbol y tenis.
    Añadidos sus metodos correspondientes de consulta
    Vista estadisticas adaptada con tablas(Hay que Cambiarlas) para que se vean los datos correspondientes
    En equipo hay que quitar la id y mostrar el nombre

--- estadistica.php
+++ estadistica.php
@@ -1,3 +1,23 @@
+<?php
+	require_once __DIR__.'/includes/config.php';
+	require_once __DIR__.'/includes/Usuario.php';
+	require_once __DIR__.'/includes/Jugador.php';
+	require_once __DIR__.'/includes/Equipo.php';
+	require_once __DIR__.'/includes/Estadistica.php';
+	require_once __DIR__.'/includes/Estadistica_futbol.php';
+	require_once __DIR__.'/includes/Estadistica_baloncesto.php';
+	require_once __DIR__.'/includes/Estadistica_balonmano.php';
+	require_once __DIR__.'/includes/Estadistica_beisbol.php';
+	require_once __DIR__.'/includes/Estadistica_tenis.php';
+
+	$nickname = $_SESSION['nombre'];
+	$usuario = Usuario::buscaUsuario($nickname);
+	$jugador = Jugador::getJugadorPorNombre($nickname);
+	if($jugador == NULL){
+		//MENSAJE DE QUE NO SE HA APUNTADO A NINGUN EQUIPO
+	}
+	$listaNombreEquipo =$jugador->listaEquiposPorJugador($nickname);
+  ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,24 +37,36 @@
 	  		<table>
 	  			<tr>
 	  				<th>Equipo</th>
-	  				<th>Posición</th>
-	  				<th>Faltas</th>
+	  				<th>Partidos Jugados</th>
+	  				<th>Partidos Ganados</th>
+	  				<th>Partidos Empatados</th>
+	  				<th>Partidos Perdidos</th>
 	  				<th>Goles</th>
-	  				<th>Tarjetas</th>
-	  				<th>PJ</th>
-	  				<th>PG</th>
-	  				<th>PP</th>
+	  				<th>Asistencias</th>
+	  				<th>Tarjetas Amarillas</th>
+	  				<th>Tarjetas Rojas</th>
 	  			</tr>
-	  			<tr>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  			</tr>
+	  				<?php
+	  					$listaEquipos = array();
+	  					$listaEstadisticas = Estadistica_futbol::listaEstadisticas($usuario->id());
+	  					if($listaEstadisticas != NULL){
+		  					foreach ($listaEstadisticas as $key) {
+		  						echo '<tr>';
+			  						//$equipo = Equipo::getInfoPorNombre($key);
+									$ObjestadisticaF = json_decode($key);
+			  						echo '<td>'.$ObjestadisticaF->id_estadistica.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidosj.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidosg.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidose.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidosp.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->goles.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->asistencias.'</td>';	
+			  						echo '<td>'.$ObjestadisticaF->tarjetaA.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->tarjetaR.'</td>';
+		  						echo '<tr>';
+		  					}
+	  					}
+	  				?>
 	  		</table>
 	  	</div>
 
@@ -43,22 +75,36 @@
 	  		<table>
 	  			<tr>
 	  				<th>Equipo</th>
-	  				<th>Posición</th>
+	  				<th>Partidos Jugados</th>
+	  				<th>Partidos Ganados</th>
+	  				<th>Partidos Empatados</th>
+	  				<th>Partidos Perdidos</th>
 	  				<th>Puntos</th>
+	  				<th>Asistencias</th>
+	  				<th>Tapones</th>
 	  				<th>Faltas</th>
-	  				<th>PJ</th>
-	  				<th>PG</th>
-	  				<th>PP</th>
 	  			</tr>
-	  			<tr>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  			</tr>
+					<?php
+	  					$listaEquipos = array();
+	  					$listaEstadisticas = Estadistica_baloncesto::listaEstadisticas($usuario->id());
+	  					if($listaEstadisticas != NULL){
+		  					foreach ($listaEstadisticas as $key) {
+		  						echo '<tr>';
+			  						//$equipo = Equipo::getInfoPorNombre($key);
+									$ObjestadisticaF = json_decode($key);
+			  						echo '<td>'.$ObjestadisticaF->id_estadistica.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidosj.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidosg.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidose.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->partidosp.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->puntos.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->asistencias.'</td>';	
+			  						echo '<td>'.$ObjestadisticaF->tapones.'</td>';
+			  						echo '<td>'.$ObjestadisticaF->faltas.'</td>';
+		  						echo '<tr>';
+		  					}
+	  					}
+	  				?>
 	  		</table>
 	  	</div>
 
@@ -67,24 +113,34 @@
 	  		<table>
 	  			<tr>
 	  				<th>Equipo</th>
-	  				<th>Posición</th>
+	  				<th>Partidos Jugados</th>
+	  				<th>Partidos Ganados</th>
+	  				<th>Partidos Empatados</th>
+	  				<th>Partidos Perdidos</th>
 	  				<th>Strike</th>
 	  				<th>Homerun</th>
-	  				<th>Tarjetas</th>
-	  				<th>PJ</th>
-	  				<th>PG</th>
-	  				<th>PP</th>
+	  				<th>Eliminaciones</th>
 	  			</tr>
-	  			<tr>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  			</tr>
+	  			<?php
+	  				$listaEquipos = array();
+	  				$listaEstadisticas = Estadistica_beisbol::listaEstadisticas($usuario->id());
+	  				if($listaEstadisticas != NULL){
+		  				foreach ($listaEstadisticas as $key) {
+		  					echo '<tr>';
+			  					//$equipo = Equipo::getInfoPorNombre($key);
+								$ObjestadisticaF = json_decode($key);
+			  					echo '<td>'.$ObjestadisticaF->id_estadistica.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosj.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosg.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidose.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosp.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->strike.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->homerun.'</td>';	
+			  					echo '<td>'.$ObjestadisticaF->eliminaciones.'</td>';
+		  					echo '<tr>';
+		  				}
+	  				}
+	  			?>
 	  		</table>
 	  	</div>
 
@@ -93,24 +149,43 @@
 	  		<table>
 	  			<tr>
 	  				<th>Equipo</th>
-	  				<th>Posición</th>
-	  				<th>Faltas</th>
+	  				<th>Partidos Jugados</th>
+	  				<th>Partidos Ganados</th>
+	  				<th>Partidos Empatados</th>
+	  				<th>Partidos Perdidos</th>
 	  				<th>Goles</th>
-	  				<th>Tarjetas</th>
-	  				<th>PJ</th>
-	  				<th>PG</th>
-	  				<th>PP</th>
+	  				<th>Asistencias</th>
+	  				<th>Tapones</th>
+	  				<th>Faltas</th>
+	  				<th>Tarjetas Amarillas</th>
+	  				<th>Tarjetas Rojas</th>
+	  				<th>Expulsion</th>
 	  			</tr>
-	  			<tr>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  			</tr>
+	  			<?php
+	  				$listaEquipos = array();
+	  				$listaEstadisticas = Estadistica_balonmano::listaEstadisticas($usuario->id());
+	  				if($listaEstadisticas != NULL){
+		  				foreach ($listaEstadisticas as $key) {
+		  					echo '<tr>';
+			  					//$equipo = Equipo::getInfoPorNombre($key);
+								$ObjestadisticaF = json_decode($key);
+			  					echo '<td>'.$ObjestadisticaF->id_estadistica.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosj.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosg.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidose.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosp.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->goles.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->asistencias.'</td>';	
+			  					echo '<td>'.$ObjestadisticaF->tapones.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->faltas.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->tarjetaA.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->tarjetaR.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->expulsion.'</td>';
+		  					echo '<tr>';
+		  				}
+	  				}
+	  			?>
+
 	  		</table>
 	  	</div>
 
@@ -119,20 +194,40 @@
 	  		<table>
 	  			<tr>
 	  				<th>Equipo</th>
-	  				<th>Sets</th>
+	  				<th>Partidos Jugados</th>
+	  				<th>Partidos Ganados</th>
+	  				<th>Partidos Empatados</th>
+	  				<th>Partidos Perdidos</th>
 	  				<th>Puntos</th>
-	  				<th>PJ</th>
-	  				<th>PG</th>
-	  				<th>PP</th>
+	  				<th>Sets</th>
+	  				<th>Juegos</th>
+	  				<th>Aces</th>
+	  				<th>Dobles Faltas</th>
+	  				<th>Errores no forzados</th>
 	  			</tr>
-	  			<tr>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  				<td>-</td>
-	  			</tr>
+	  			<?php
+	  				$listaEquipos = array();
+	  				$listaEstadisticas = Estadistica_tenis::listaEstadisticas($usuario->id());
+	  				if($listaEstadisticas != NULL){
+		  				foreach ($listaEstadisticas as $key) {
+		  					echo '<tr>';
+			  					//$equipo = Equipo::getInfoPorNombre($key);
+								$ObjestadisticaF = json_decode($key);
+			  					echo '<td>'.$ObjestadisticaF->id_estadistica.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosj.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosg.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidose.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->partidosp.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->puntos.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->sets.'</td>';	
+			  					echo '<td>'.$ObjestadisticaF->juegos.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->aces.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->dobles_faltas.'</td>';
+			  					echo '<td>'.$ObjestadisticaF->errores.'</td>';
+		  					echo '<tr>';
+		  				}
+	  				}
+	  			?>
 	  		</table>
 	  	</div>
 	</div>
---
+++ includes/Estadistica_tenis.php
@@ -0,0 +1,131 @@
+<?php
+require_once __DIR__ . '/Aplicacion.php';
+require_once __DIR__ . '/Estadistica.php';
+
+class Estadistica_tenis extends Estadistica
+{
+    //Busca las estadisticas especificas de un usuario que tiene un jugador en un equipo de futbol correspondiente
+    public static function buscaEstadisticaPorEquipo($idusuario, $idequipo)
+    {
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query = sprintf("SELECT * FROM estadisticas_tenis WHERE es_usuario = '%s' AND es_equipo = '%s'",
+                    $conn->real_escape_string($idusuario)
+                    , $conn->real_escape_string($idequipo));
+        $rs = $conn->query($query);
+        $result = false;
+        if ($rs) {
+            if ( $rs->num_rows == 1) {
+                $fila = $rs->fetch_assoc();
+                $estadistica = new Estadistica_tenis($fila['id_estenis'], $fila['es_usuario'], $fila['es_equipo'], $fila['pj_usuario'], $fila['pg_usuario'], $fila['pe_usuario'], $fila['pp_usuario'], $fila['puntos_usuario'], $fila['sets'], $fila['juegos'], $fila['aces'], $fila['dobles_faltas'], $fila['errores_no_forzados']);
+                $result = json_encode(get_object_vars($estadistica));
+            }
+            $rs->free();
+        } else {
+            echo "Error al consultar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $result;
+    }
+
+    //Lista todas las estadisticas que tiene referido un usuario sobre el futbol
+    public static function listaEstadisticas($idusuario){
+        $app = Aplicacion::getSingleton();
+        $conn = $app->conexionBd();
+        $query = sprintf("SELECT * FROM estadisticas_tenis es JOIN equipos e ON es.es_equipo = e.id_equipo WHERE es.es_usuario = '%s'", $conn->real_escape_string($idusuario));
+       $rs =$conn->query($query);
+       if ( $rs ) {
+            if($rs->num_rows == 0)
+                $lista = null;
+            else {
+                $lista = array();
+                while ($fila = $rs->fetch_assoc()) {
+                    $aux = new Estadistica_tenis($fila['id_estenis'], $fila['es_usuario'], $fila['es_equipo'], $fila['pj_usuario'], $fila['pg_usuario'], $fila['pe_usuario'], $fila['pp_usuario'], $fila['puntos_usuario'], $fila['sets'], $fila['juegos'], $fila['aces'], $fila['dobles_faltas'], $fila['errores_no_forzados']);
+                     $json = json_encode(get_object_vars($aux));
+                    array_push($lista, $json);
+                }
+                $rs->free();
+               
+            }
+        } else {
+            echo "Error al consultar la base de datos: (" . $conn->errno . ") " . utf8_encode($conn->error);
+            exit();
+        }
+        return $lista;
+    }
+    
+
+//Clase Estadistica
+
+
+    private $puntos;
+
+    private $sets;
+
+    private $juegos;
+
+    private $aces;
+
+    private $dobles_faltas;
+
+    private $errores;
+
+    public function __construct($id_estadistica, $usuario, $equipo, $partidosj, $partidosg, $partidose, $partidosp, $puntos, $sets, $juegos, $aces, $dobles_faltas, $errores)
+    {
+        parent::__construct($id_estadistica, $usuario, $equipo, $partidosj, $partidosg, $partidose, $partidosp);
+        $this->puntos = $puntos;
+        $this->sets = $sets;
+        $this->juegos = $juegos;
+        $this->aces = $aces;
+        $this->dobles_faltas = $dobles_faltas;
+        $this->errores = $errores;
+    }
+
+    public function getPuntos(){
+        return $this->puntos;
+    }
+
+    public function getSets(){
+        return $this->sets;
+    }
+
+    public function getJuegos(){
+        return $this->juegos;
+    }
+
+    public function getAces(){
+        return $this->aces;
+    }
+
+    public function getFaltas(){
+        return $this->dobles_faltas;
+    }
+
+    public function getErrores(){
+        return $this->errores;
+    }
+
+    public function setPuntos($puntos){
+        $this->puntos = $puntos;
+    }
+
+    public function setSets($sets){
+        $this->sets = $sets;
+    }
+
+    public function setJuegos($juegos){
+        $this->juegos = $juegos;
+    }
+
+    public function setAces($aces){
+        $this->aces = $aces;
+    }
+
+    public function setFaltas($dobles_faltas){
+        $this->dobles_faltas = $dobles_faltas;
+    }
+
+    public function setErrores($errores){
+        $this->errores = $errores;
+    }
+}
